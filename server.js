975c1a5a0fcaf2edd32e3d68a702cd4e03d4b469
Neil Chauhan <neilchauhan2@gmail.com>
2020-10-07 19:54:49 +0530

    initial set up

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+
+const app = express();
+
+app.use("/graphql", graphqlHTTP({}));
+
+app.listen(8000, () => console.log("Server started oh http://localhost:8000"));
